3c5cf8e2d154bb9bf285189648c5c5dd2040d78c
Jake Sanderson <jake.sanderson@mymail.champlain.edu>
2024-11-06 15:33:57 -0500

    finished first draft of particle simulation

--- Hydraulic Erosion/Assets/Scripts/HydraulicErosion.cs
+++ Hydraulic Erosion/Assets/Scripts/HydraulicErosion.cs
@@ -15,8 +15,12 @@ public class HydraulicErosion : MonoBehaviour
 
     //Particle data
     [SerializeField]
-    private int mMaxParticles;
+    private int mMaxParticles = 1000;
     private int currentParticle = 0;
+
+    //Erosion data
+    float depositionRate = 0.1f;
+    float evaporationRate = 0.001f;
 
     struct Particle
     {
@@ -52,17 +56,19 @@ public class HydraulicErosion : MonoBehaviour
             if (mIsEroding)
             {
                 mIsEroding = false;
+                Debug.Log("Not Eroding");
             }
             else
             {
                 mIsEroding = true;
+                Debug.Log("Eroding");
             }
         }
     }
 
     void FixedUpdate()
     {
-        if (currentParticle < mMaxParticles)
+        if (currentParticle < mMaxParticles && mIsEroding)
         {
             Erode();
             currentParticle++;
@@ -71,7 +77,7 @@ public class HydraulicErosion : MonoBehaviour
 
     private void Erode()
     {
-        Vector2 startPos = new Vector2(Random.Range(1.0f, (float)mSideSize), Random.Range(1.0f, (float)mSideSize));
+        Vector2 startPos = new Vector2(Random.Range(1.0f, (float)mSideSize - 1), Random.Range(1.0f, (float)mSideSize - 1));
         Particle drop = new Particle(startPos);
 
         //While there is still water in drop
@@ -87,13 +93,30 @@ public class HydraulicErosion : MonoBehaviour
             Vector2 lowestDirection = nextLowest - currentPos;
 
             //Change particle speed and pos
+            drop.mSpeed += Time.deltaTime * (lowestDirection / drop.mVolume); //a = F/m
+            drop.mPos += Time.deltaTime * drop.mSpeed;
 
+            if (currentPos.x <= 0 || currentPos.y <= 0 || currentPos.x >= mSideSize || currentPos.y >= mSideSize) break;
 
             //Calculate sediment capacity
+            float maxSed = drop.mVolume * drop.mSpeed.magnitude * (mHeights[(int)currentPos.y, (int)currentPos.x] - mHeights[(int)drop.mPos.y, (int)drop.mPos.x]);
+            if (maxSed < 0.0f)
+            {
+                maxSed = 0.0f;
+            }
 
+            float sedDiff = maxSed - drop.mSediment;
 
             //Change heightmap and drop properties
+            drop.mSediment += Time.deltaTime * sedDiff * depositionRate;
+            mHeights[(int)currentPos.y, (int)currentPos.x] -= Time.deltaTime * drop.mVolume * sedDiff * depositionRate;
+            Debug.Log("Changed (" + (int)currentPos.x + ", " + (int)currentPos.y + ") by " + (Time.deltaTime * drop.mVolume * sedDiff * depositionRate));
+
+            //Evaporate drop
+            drop.mVolume *= (1.0f - Time.deltaTime * evaporationRate);
         }
+
+        mTerrain.terrainData.SetHeights(0, 0, mHeights);
     }
 
     private Vector2 lowestNeighbor(Vector2 _pos)
@@ -104,6 +127,8 @@ public class HydraulicErosion : MonoBehaviour
         {
             for (int x = -1; x <= 1; x++)
             {
+                if (_pos.x + x < 0 || _pos.y + y < 0 || _pos.x + x >= mSideSize || _pos.y + y >= mSideSize) continue;
+
                 //Check if neighbor is lower than the current lowest
                 if ((x != 0 && y != 0) && mHeights[(int)_pos.y + y, (int)_pos.x + x] < mHeights[(int)lowestNeighbor.y, (int)lowestNeighbor.x])
                 {
