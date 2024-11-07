f9ab15fd555e65042e1262ed8c0d5311671bf64d
Jake Sanderson <jake.sanderson@mymail.champlain.edu>
2024-11-07 12:40:04 -0500

    Erosion works now. Smoothing implemented

--- Hydraulic Erosion/Assets/Scripts/HydraulicErosion.cs
+++ Hydraulic Erosion/Assets/Scripts/HydraulicErosion.cs
@@ -16,6 +16,8 @@ public class HydraulicErosion : MonoBehaviour
     //Particle data
     [SerializeField]
     private int mMaxParticles;
+    public static int mNumIterations = 100;
+    private int mCurrentParticle = 0;
 
     //Erosion data
     [Range(0f, 1f)]
@@ -35,6 +37,7 @@ public class HydraulicErosion : MonoBehaviour
 
             mVolume = 1.0f;
             mSediment = 0.0f;
+            mIterations = mNumIterations;
         }
 
         public Vector2 mPos;
@@ -43,6 +46,7 @@ public class HydraulicErosion : MonoBehaviour
 
         public float mVolume;
         public float mSediment;
+        public float mIterations;
     }
 
     // Start is called before the first frame update
@@ -51,8 +55,6 @@ public class HydraulicErosion : MonoBehaviour
         mTerrain = GetComponent<Terrain>();
         mSideSize = mTerrain.terrainData.heightmapResolution;
         mHeights = mTerrain.terrainData.GetHeights(0, 0, mSideSize, mSideSize);
-
-        SimulateErosion();
     }
 
     // Update is called once per frame
@@ -60,28 +62,23 @@ public class HydraulicErosion : MonoBehaviour
     {
         if (Input.GetKeyDown("e"))
         {
-            if (mIsEroding)
-            {
-                mIsEroding = false;
-                Debug.Log("Not Eroding");
-            }
-            else
-            {
-                mIsEroding = true;
-                Debug.Log("Eroding");
-            }
+            mIsEroding = !mIsEroding;
+            Debug.Log(mIsEroding ? "Eroding" : "Not Eroding");
         }
     }
 
     void FixedUpdate()
     {
-    }
-
-    private void SimulateErosion()
-    {
-        for (int i = 0; i < mMaxParticles; i++)
+        if (mCurrentParticle < mMaxParticles && mIsEroding)
         {
             Erode();
+            if (mCurrentParticle % 50 == 0) Debug.Log("Particle Num: " +  mCurrentParticle);
+            mCurrentParticle++;
+        }
+        else if (mCurrentParticle == mMaxParticles)
+        {
+            SmoothTerrain();
+            mCurrentParticle++;
         }
     }
 
@@ -89,33 +86,35 @@ public class HydraulicErosion : MonoBehaviour
     {
         Vector2 startPos = new Vector2(Random.Range(1.0f, (float)mSideSize - 1), Random.Range(1.0f, (float)mSideSize - 1));
         Particle drop = new Particle(startPos);
+        HashSet<Vector2> visitedPositions = new HashSet<Vector2>();
 
         //While there is still water in drop
-        while (drop.mVolume > 0.01f)
+        while (drop.mIterations > 0)
         {
             Vector2 currentPos = new Vector2(drop.mPos.x, drop.mPos.y);
-            Debug.Log("Pos: " + currentPos);
 
             if (currentPos.x <= 1 || currentPos.y <= 1 || currentPos.x >= mSideSize - 1 || currentPos.y >= mSideSize - 1) break;
 
             //Calculate steepest direction
             Vector2 lowestDirection = calcFlowDirection(currentPos);
-            Debug.Log("Lowest Direction: " + lowestDirection);
 
             if (lowestDirection == Vector2.zero) break;
 
             //Change particle speed and pos
-            drop.mVelocity = /*Time.deltaTime * */(lowestDirection); //a = F/m
-            drop.mPos += /*Time.deltaTime **/ drop.mVelocity * drop.mSpeed;
+            drop.mVelocity =lowestDirection;
+            drop.mPos += drop.mVelocity * drop.mSpeed;
 
             if (drop.mPos.x <= 0 || drop.mPos.y <= 0 || drop.mPos.x >= mSideSize || drop.mPos.y >= mSideSize) break;
+
+            //Check if visited already to prevent spikes and pits
+            if (visitedPositions.Contains(new Vector2Int((int)drop.mPos.x, (int)drop.mPos.y))) break;
+            visitedPositions.Add(new Vector2Int((int)drop.mPos.x, (int)drop.mPos.y));
 
             //Calculate sediment capacity
             float terrainHeight = mTerrain.terrainData.size.y;
             float oldHeight = calcInterpolatedHeight(currentPos);
             float newHeight = calcInterpolatedHeight(drop.mPos);
             float deltaHeight = oldHeight - newHeight;
-            Debug.Log("DeltaH: " + deltaHeight);
 
             float sedCapacity = Mathf.Max(-deltaHeight * drop.mSpeed * drop.mVolume, minSedCapacity);
 
@@ -124,6 +123,7 @@ public class HydraulicErosion : MonoBehaviour
             {
                 //Erode terrain
                 float amountToErode = Mathf.Min(sedCapacity - drop.mSediment, -(deltaHeight));
+                if (amountToErode > 0.001f) amountToErode = 0.001f;
 
                 drop.mSediment += amountToErode;
                 mHeights[(int)currentPos.y, (int)currentPos.x] -= amountToErode;
@@ -132,6 +132,8 @@ public class HydraulicErosion : MonoBehaviour
             {
                 //Desposit sediment
                 float amountToDeposit = (deltaHeight > 0) ? Mathf.Min(deltaHeight, drop.mSediment) : drop.mSediment - sedCapacity;
+                if (amountToDeposit  > 0.001f) amountToDeposit = 0.001f;
+
                 drop.mSediment -= amountToDeposit;
                 mHeights[(int)currentPos.y, (int)currentPos.x] += amountToDeposit;
             }
@@ -139,34 +141,11 @@ public class HydraulicErosion : MonoBehaviour
             //Evaporate drop
             drop.mSpeed = Mathf.Sqrt(drop.mSpeed * drop.mSpeed + deltaHeight * 4); //Magic number is "gravity" multiplier
             drop.mVolume *= (1.0f - 1 * (evaporationRate));
+            drop.mIterations--;
         }
 
         mTerrain.terrainData.SetHeights(0, 0, mHeights);
     }
-
-    private Vector2 lowestNeighbor(Vector2 _pos)
-    {
-        Vector2 lowestNeighbor = _pos;
-
-        for (int y = -1; y <= 1; y++)
-        {
-            for (int x = -1; x <= 1; x++)
-            {
-                //Check against bounds
-                if (_pos.x + x < 0 || _pos.y + y < 0 || _pos.x + x >= mSideSize || _pos.y + y >= mSideSize) continue;
-
-                //Check if neighbor is lower than the current lowest
-                if ((x != 0 && y != 0) && mHeights[(int)_pos.y + y, (int)_pos.x + x] < mHeights[(int)lowestNeighbor.y, (int)lowestNeighbor.x])
-                {
-                    Vector2 tmpVec = new Vector2(x, y);
-                    lowestNeighbor = tmpVec + _pos;
-                }
-            }
-        }
-
-        return lowestNeighbor;
-    }
-
     private Vector2 calcFlowDirection(Vector2 _pos)
     {
         Vector2Int coordPos = new Vector2Int((int)_pos.x, (int)_pos.y);
@@ -234,5 +213,30 @@ public class HydraulicErosion : MonoBehaviour
         float height = topLeftHeight * (1 - x) * (1 - y) + topRightHeight * x * (1 - y) + bottomLeftHeight * (1 - x) * y + bottomRightHeight * x * y;
 
         return height;
+    }
+
+    private void SmoothTerrain()
+    {
+        for (int y = 1; y < mSideSize - 1; y++)
+        {
+            for (int x = 1; x < mSideSize - 1; x++)
+            {
+                float avgHeight = 0;
+
+                for (int i = -1; i <= 1; i++)
+                {
+                    for(int j = -1; j <= 1; j++)
+                    {
+                        avgHeight += mHeights[y + i, x + j];
+                    }
+                }
+
+                avgHeight /= 9;
+                mHeights[y, x] = avgHeight;
+            }
+        }
+
+        mTerrain.terrainData.SetHeights(0, 0, mHeights);
+        Debug.Log("Smoothed");
     }
 }
